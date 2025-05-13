ee21964cdd12f7a3c234221bd05438d202a4f73e
Akshit Lakhanpal <akshitworkspace@gmail.com>
2025-05-13 16:54:37 +0530

    ver:ver1.0.1

--- dist/useQuery.d.ts
+++
@@ -1,6 +0,0 @@
-export default function useQuery(): {
-    removeParams: any;
-    setParams: any;
-    getParams: any;
-    removeAllParams: any;
-};
--- src/useProQuery.ts
+++ src/useProQuery.ts
@@ -1,37 +1,37 @@
 'use client';
 import { useCallback } from "react";
-export default function useQuery() {
-    const updateURL = useCallback((updatedParams) => {
-        if (typeof window === "undefined")
-            return;
+
+export default function useProQuery() {
+    const updateURL = useCallback((updatedParams: URLSearchParams) => {
+        if (typeof window === "undefined") return;
         window.history.replaceState(null, '', `${window.location.pathname}?${updatedParams.toString()}`);
     }, []);
-    const removeParams = useCallback((paramName) => {
-        if (typeof window === "undefined")
-            return;
+
+    const removeParams = useCallback((paramName: string) => {
+        if (typeof window === "undefined") return;
         const updatedParams = new URLSearchParams(window.location.search);
         updatedParams.delete(paramName);
         updateURL(updatedParams);
     }, [updateURL]);
-    const setParams = useCallback((paramName, paramValue) => {
-        if (typeof window === "undefined")
-            return;
+
+    const setParams = useCallback((paramName: string, paramValue: string) => {
+        if (typeof window === "undefined") return;
         const updatedParams = new URLSearchParams(window.location.search);
         updatedParams.set(paramName, paramValue);
         updateURL(updatedParams);
     }, [updateURL]);
-    const getParams = useCallback((paramName) => {
-        if (typeof window === "undefined")
-            return '';
+
+    const getParams = useCallback((paramName: string) => {
+        if (typeof window === "undefined") return '';
         const location = window.location.href;
         const url = new URL(location);
         return url.searchParams.get(paramName) || '';
     }, []);
+
     const removeAllParams = useCallback(() => {
-        if (typeof window === "undefined")
-            return;
+        if (typeof window === "undefined") return;
         window.history.replaceState(null, '', window.location.pathname);
-    }, []);
+    }, [])
+
     return { removeParams, setParams, getParams, removeAllParams };
-}
-;
+};
