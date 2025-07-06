18cb9bc60029ce54130f24521f36e26056bcea8c
akshitlakhanpal <akshitworkspace@gmail.com>
2025-07-06 17:02:49 +0530

    Feat:now dev can opt for preserving search params history

--- src/useProQuery.ts
+++ src/useProQuery.ts
@@ -1,15 +1,18 @@
 'use client';
 import { useCallback } from "react";
 
-export default function useProQuery() {
-    const updateURL = useCallback((updatedParams: URLSearchParams) => {
+export default function useProQuery(preserveHistory?: boolean) {
+    const updateURL = useCallback((updatedParams: URLSearchParams, removeAll?: boolean) => {
         if (typeof window === "undefined") return;
-        window.history.replaceState(null, '', `${window.location.pathname}?${updatedParams.toString()}`);
-    }, []);
+        const update = preserveHistory ? window.history.pushState : window.history.replaceState;
+        const url = removeAll ? window.location.pathname : `${window.location.pathname}?${updatedParams.toString()}`;
+        update(null, '', url);
+    }, [preserveHistory]);
 
     const removeParams = useCallback((paramName: string) => {
         if (typeof window === "undefined") return;
         const updatedParams = new URLSearchParams(window.location.search);
+        if (!updatedParams.has(paramName)) return;
         updatedParams.delete(paramName);
         updateURL(updatedParams);
     }, [updateURL]);
@@ -30,8 +33,10 @@ export default function useProQuery() {
 
     const removeAllParams = useCallback(() => {
         if (typeof window === "undefined") return;
-        window.history.replaceState(null, '', window.location.pathname);
-    }, [])
+        const searchParams = new URLSearchParams(window.location.search);
+        if (!searchParams.size) return;
+        updateURL(searchParams, true);
+    }, [updateURL])
 
     return { removeParams, setParams, getParams, removeAllParams };
 };
